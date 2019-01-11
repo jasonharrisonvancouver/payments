11fcd30eb4a4af3622f16ced0c5b61d305e4fbbc
Jason Harrison <jasonharrisonvancouver@gmail.com>
2019-01-11 15:11:56 -0800

    created PaymentGateway class

---
+++ Payments/PaymentGateway.h
@@ -0,0 +1,17 @@
+//
+//  PaymentGateway.h
+//  Payments
+//
+//  Created by jason harrison on 2019-01-11.
+//  Copyright © 2019 jason harrison. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PaymentGateway : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Payments/PaymentGateway.m
@@ -0,0 +1,13 @@
+//
+//  PaymentGateway.m
+//  Payments
+//
+//  Created by jason harrison on 2019-01-11.
+//  Copyright © 2019 jason harrison. All rights reserved.
+//
+
+#import "PaymentGateway.h"
+
+@implementation PaymentGateway
+
+@end

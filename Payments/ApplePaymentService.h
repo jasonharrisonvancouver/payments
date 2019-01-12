071d08a60a5a651233bcf5ef434b5eb57301aa36
Jason Harrison <jasonharrisonvancouver@gmail.com>
2019-01-11 16:12:07 -0800

    added apple

---
+++ Payments/ApplePaymentService.h
@@ -0,0 +1,17 @@
+//
+//  ApplePaymentService.h
+//  Payments
+//
+//  Created by jason harrison on 2019-01-11.
+//  Copyright © 2019 jason harrison. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ApplePaymentService : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Payments/ApplePaymentService.m
@@ -0,0 +1,13 @@
+//
+//  ApplePaymentService.m
+//  Payments
+//
+//  Created by jason harrison on 2019-01-11.
+//  Copyright © 2019 jason harrison. All rights reserved.
+//
+
+#import "ApplePaymentService.h"
+
+@implementation ApplePaymentService
+
+@end
